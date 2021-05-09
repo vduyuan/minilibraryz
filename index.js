80e8c9365c97e64c5f31235b0e6c4ec6827c6872
vduyuan <vincent.duyuan@gmail.com>
2021-05-08 21:54:29 -0400

    test-heroku

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
 const mongoose = require('mongoose'); 
-const axios = require('axios'); 
+const axios = require('axios');
+const path = require('path'); 
 
 const app = express(); 
 
@@ -12,12 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors()); 
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'index.js'));
-})
-
 const port = process.env.PORT || 5000;
-app.listen(5000); 
+app.listen(port); 
 
 mongoose.connect(process.env.MONGO_URI,  {useNewUrlParser: true, useUnifiedTopology: true })
     .then(console.log("Server running on port " + port));
@@ -27,12 +24,11 @@ const userRoutes = require('./routes/users');
 const collageRoutes = require('./routes/collages');
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('client/build'));  
 }
 
+app.use(express.static('client/build'));  
 app.use('/users', userRoutes);
 app.use('/collages', collageRoutes);
 
-/* app.get('/', (req, res) => {
-    res.send("Hello"); 
-}) */
+
